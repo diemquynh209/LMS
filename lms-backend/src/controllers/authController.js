c517b53c566afd70321b693cea897e04ea3043d0
Diem Quynh <diemquynh20092k1@gmail.com>
2026-03-17 22:42:48 +0700

    tao model dang ky

--- lms-backend/src/controllers/authController.js
+++ lms-backend/src/controllers/authController.js
@@ -3,6 +3,41 @@ const userModel = require('../models/userModel');
 const { json } = require('express');
 const registerUser = async(req,res)=>{
     const{full_name,email,password,invite_code}= req.body;
-    res.status(201),json({message:"Ban da dang ky thanh cong"});
-};
+    if (!full_name||!email||!password){
+        return res.status(400).json({message:"Vui long khong de trong"})
+    }
+    try{
+        const existingUser = await userModel.findUserByEmail(email);
+        if (existingUser){
+            return res.status(400).json({message:"email ton tai"});
+        }
+        let role ='Student';
+
+        if (invite_code) {
+            const validInvite = await userModel.checkInviteCode(email, invite_code);
+            if (!validInvite) {
+                return res.status(400).json({ message: "ma moi khong hop le" });
+            }
+            role = 'Instructor';
+        }
+        //hash pass
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        await userModel.createUser(full_name, email, hashedPassword, role);
+        
+        //xoa ma moi da dung
+        if (role === 'Instructor') {
+            await userModel.burnInviteCode(email, invite_code);
+        }
+
+        return res.status(201).json({ 
+            message: "Dang ky thanh cong!",
+            role: role
+        });
+
+    } catch (error) {
+        console.error("Lỗi đăng ký:", error);
+        return res.status(500).json({ message: "Lỗi máy chủ!" });
+    }
+    };
 module.exports={registerUser};
--- lms-backend/src/models/userModel.js
+++ lms-backend/src/models/userModel.js
@@ -7,5 +7,13 @@ const createUser = async(fullName,email,password,role)=>{
     const[result]=await pool.query('INSERT INTO Users(full_name,email,password_hash,role) VALUES (?,?,?,?)',[fullName,email,hashesPassword,role]);
     return result;
 };
+//xu ly ma moi cho gvien
+const checkInviteCode = async (email,inviteCode)=>{
+    const[rows]= await pool.query('SELECT*FROM Instructor_Invites WHERE email = ? AND invite_code = ? AND is_used = FALSE',[email, inviteCode]);
+    return rows[0];
+};
+const burnInviteCode =async(email, inviteCode)=>{
+    await pool.query('UPDATE Instructor_Invites SET is_used = TRUE WHERE email = ? AND invite_code =?', [email,inviteCode]);
+};
 
-module.exports = {findByEmail,createUser};
+module.exports = {findByEmail,createUser,checkInviteCode,burnInviteCode};
