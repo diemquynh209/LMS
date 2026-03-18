9f0a3577c1ec71c80f58a06f6e7e6460a29cf8fb
Diem Quynh <diemquynh20092k1@gmail.com>
2026-03-18 13:31:57 +0700

    fix bien password

--- lms-backend/src/models/userModel.js
+++ lms-backend/src/models/userModel.js
@@ -3,8 +3,8 @@ const findUserByEmail = async(email)=>{
     const [rows]=await pool.query('SELECT*FROM Users WHERE email =?',[email]);
     return rows[0];
 };
-const createUser = async(fullName,email,password,role)=>{
-    const[result]=await pool.query('INSERT INTO Users(full_name,email,password_hash,role) VALUES (?,?,?,?)',[fullName,email,password,role]);
+const createUser = async(fullName,email,hashedPassword,role)=>{
+    const[result]=await pool.query('INSERT INTO Users(full_name,email,password_hash,role) VALUES (?,?,?,?)',[fullName,email,hashedPassword,role]);
     return result;
 };
 //xu ly ma moi cho gvien
